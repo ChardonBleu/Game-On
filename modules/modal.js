38278fa39dd889a25e44f92a7f5f58b200721ed7
ChardonBleu <f.rondi@orange.fr>
2025-03-30 09:22:10 +0200

    ♻️ refactor: refacto js files

---
+++ modules/modal.js
@@ -0,0 +1,9 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const formData = document.querySelectorAll(".formData");
+
+
+// launch modal form
+export function launchModal() {
+  modalbg.style.display = "block";
+}
