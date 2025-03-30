e5a8cc7d1a6c9a3ed53019230e263208e156de54
ChardonBleu <f.rondi@orange.fr>
2025-03-30 13:03:30 +0200

    ✨ feat: add close modal

--- modules/modal.js
+++ modules/modal.js
@@ -1,8 +1,12 @@
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const formData = document.querySelectorAll(".formData");
+// const formData = document.querySelectorAll(".formData");
 
 // launch modal form
 export function launchModal() {
   modalbg.style.display = "block";
+}
+
+export function closeModal() {
+  modalbg.style.display = "none";
 }
