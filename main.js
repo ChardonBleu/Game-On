0f5db20194a6f8e4cd35ae9cbaab199f266d7de5
ChardonBleu <f.rondi@orange.fr>
2025-03-31 12:54:26 +0200

    ✨ feat: animation on modal close and activation navbar links

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import { launchModal, closeModal } from "./modules/modal.js";
-import { editNav } from "./modules/nav.js";
+import { editNav, navLinkActive } from "./modules/nav.js";
 
 // DOM Elements
 const navIcon = document.querySelector(".icon");
+const navLinks = document.querySelectorAll('.main-navbar a');
 const modalBtn = document.querySelectorAll(".modal-btn");
-const closeBtn = document.querySelector(".close")
+const closeBtn = document.querySelector(".close");
 
 // display mobile nav
 navIcon.addEventListener("click", (event) => {
@@ -12,6 +13,12 @@ navIcon.addEventListener("click", (event) => {
   editNav();
 });
 
+navLinks.forEach((link) => link.addEventListener("click", (event) => {
+  navLinkActive(event);
+}));
+
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-closeBtn.addEventListener("click", closeModal)
+
+closeBtn.addEventListener("click", closeModal);
